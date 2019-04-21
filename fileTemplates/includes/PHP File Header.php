356bf12dd4813e727fbbcc6defb9c2526db0a91e
senyiwen <senyiwen@windows10.microdone.cn>
2019-04-21 11:04:32 +0800

    PS-2019.1.1 <senyiwen@DESKTOP-F360Q0E Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, monokai.icls, WarmNeon copy.icls, project.default.xml, Zen CSS.xml, Default.xml, Twig.xml, file.template.settings.xml, find.xml, ui.lnf.xml, Darcula copy2.icls, filetypes.xml, php.xml, xsl.xml, code.style.schemes.xml, colors.scheme.xml, html_xml.xml, Zen HTML.xml, databaseDrivers.xml, templates.xml, PHP Function Doc Comment.php, PHP.xml, JavaScript.xml, laf.xml, PHP File Header.php, Zen XSL.xml, Default _1_.xml, editor.codeinsight.xml, databaseSettings.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,6 @@
+/**
+ * Created by ${PRODUCT_NAME}.
+ * User: niaoyoo
+ * Date: ${DATE}
+ * Time: ${TIME}
+ */
---
+++ fileTemplates/includes/PHP Function Doc Comment.php
@@ -0,0 +1,9 @@
+/**
+* ${NAME}
+${PARAM_DOC}
+#if (${TYPE_HINT} != "void") * @return ${TYPE_HINT}
+#end
+${THROWS_DOC}
+* @author NiaoYoo <email@NiaoYoo.com>
+* @dateTime ${DATE} ${TIME}
+*/
